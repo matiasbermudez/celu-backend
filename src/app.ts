8fb1feaec4f8fe0ae7c6ec0f2473299bdd06d442
matiasbermudez <mabgrc@hotmail.com>
2025-12-16 23:55:44 -0300

    Initial backend setup

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/test', (req, res) => {
+  res.send('Hello, World!');
+});
+
+
+export default app;
